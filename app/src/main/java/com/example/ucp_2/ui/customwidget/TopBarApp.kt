c2ef6a980a675985197ab4402116083e85dee72e
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-20 05:39:41 +0700

    Menambahkan button kembali

--- app/src/main/java/com/example/ucp_2/ui/customwidget/TopBarApp.kt
+++ app/src/main/java/com/example/ucp_2/ui/customwidget/TopBarApp.kt
@@ -2,8 +2,12 @@ package com.example.ucp_2.ui.customwidget
 
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,6 +26,21 @@ fun TopAppBar(
             .statusBarsPadding(),
         contentAlignment = Alignment.Center
     ) {
+        if (showBackButton) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TextButton(
+                    onClick = onBack,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                ) {
+                    Text("Kembali")
+                }
+                Spacer(modifier = Modifier.weight(2f))
+            }
+        }
 
     }
+
 }
