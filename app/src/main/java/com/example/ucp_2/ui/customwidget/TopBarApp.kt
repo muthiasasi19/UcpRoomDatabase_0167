4605707eac58198228ba145b751262547295f1e4
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-20 05:32:35 +0700

    Menambahkan parameter untuk fungsi TopAppBar

--- app/src/main/java/com/example/ucp_2/ui/customwidget/TopBarApp.kt
+++ app/src/main/java/com/example/ucp_2/ui/customwidget/TopBarApp.kt
@@ -1,2 +1,16 @@
 package com.example.ucp_2.ui.customwidget
 
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun TopAppBar(
+    onBack: () -> Unit,
+    showBackButton: Boolean = true,
+    judul: String,
+    modifier: Modifier = Modifier
+) {
+
+}
