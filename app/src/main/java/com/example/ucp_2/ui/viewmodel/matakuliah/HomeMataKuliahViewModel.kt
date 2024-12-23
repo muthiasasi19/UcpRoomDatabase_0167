563f4292c4732c6c6179bf15404e03ab3001f8b2
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-23 15:20:21 +0700

    Menambahkan class HomeMataKuliahViewModel

--- app/src/main/java/com/example/ucp_2/ui/viewmodel/matakuliah/HomeMataKuliahViewModel.kt
+++ app/src/main/java/com/example/ucp_2/ui/viewmodel/matakuliah/HomeMataKuliahViewModel.kt
@@ -1,7 +1,51 @@
 package com.example.ucp_2.ui.viewmodel.matakuliah
 
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp_2.data.entity.MataKuliah
+import com.example.ucp_2.repository.matakuliah.RepositoryMataKuliah
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeMataKuliahViewModel(
+    private val repositoryMataKuliah: RepositoryMataKuliah
+) : ViewModel() {
+    val homeUiState: StateFlow<HomeUiState> = repositoryMataKuliah.getAllMataKuliah()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMataKuliah = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}
 
 data class HomeUiState(
     val listMataKuliah: List<MataKuliah> = listOf(),
