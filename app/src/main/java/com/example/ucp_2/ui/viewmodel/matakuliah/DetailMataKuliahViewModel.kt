3d76229ac46036bafa0108a30a110a4079cfefce
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-23 15:54:25 +0700

    Membuat class DetailMataKuliahViewModel dan fungsi delete MK

--- app/src/main/java/com/example/ucp_2/ui/viewmodel/matakuliah/DetailMataKuliahViewModel.kt
+++ app/src/main/java/com/example/ucp_2/ui/viewmodel/matakuliah/DetailMataKuliahViewModel.kt
@@ -1,8 +1,65 @@
 package com.example.ucp_2.ui.viewmodel.matakuliah
 
-
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp_2.data.entity.MataKuliah
+import com.example.ucp_2.repository.matakuliah.RepositoryMataKuliah
+import com.example.ucp_2.ui.Navigasi.DestinasiDetailMataKuliah
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
+class DetailMataKuliahViewModel(
+    savedStateHandle: SavedStateHandle,   // Menyimpan data yang terkait dengan lifecycle (digunakan untuk mempertahankan state ViewModel)
+    private val RepositoryMataKuliah: RepositoryMataKuliah,
+) : ViewModel(){
+    private val _kode: String = checkNotNull(savedStateHandle[DestinasiDetailMataKuliah.KODE_MATAKULIAH])
+
+    val detailUiState: StateFlow<DetailUiState> = RepositoryMataKuliah.getDetailMataKuliah(_kode)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)  // Menambahkan delay untuk mensimulasikan proses loading
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,   // Menandakan bahwa proses loading selesai meskipun ada error
+                    isError = true,
+                    erorMessage = it.message ?: "Terjadi Kesalahan",
+
+                    )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
+
+    fun deleteMataKuliah(){  // Fungsi untuk menghapus data mata kuliah
+        detailUiState.value.detailUiEvent.toMataKuliahEntity().let {
+            viewModelScope.launch {
+                RepositoryMataKuliah.deleteMataKuliah(it)  // Memanggil fungsi repository untuk menghapus mata kuliah
+            }
+        }
+    }
+}
 
 data class DetailUiState(
     val detailUiEvent :MataKuliahEvent = MataKuliahEvent(),
