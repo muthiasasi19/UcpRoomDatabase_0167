3253de322b31998dc4a3a85406919edf6e0109f0
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-23 15:28:57 +0700

    Menambahkan class DetailDosenViewModel.kt

--- app/src/main/java/com/example/ucp_2/ui/viewmodel/dosen/DetailDosenViewModel.kt
+++ app/src/main/java/com/example/ucp_2/ui/viewmodel/dosen/DetailDosenViewModel.kt
@@ -1,7 +1,58 @@
 package com.example.ucp_2.ui.viewmodel.dosen
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp_2.data.entity.Dosen
+import com.example.ucp_2.repository.dosen.RepositoryDosen
+import com.example.ucp_2.ui.Navigasi.DestinasiDetailDosen
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+
+class DetailDosenViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDosen: RepositoryDosen,
+) : ViewModel(){
+
+    private val _nidn: String = checkNotNull(savedStateHandle[DestinasiDetailDosen.KODE_DOSEN])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryDosen.getDosen(_nidn)
+        .filterNotNull()
+        .map{
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(timeMillis = 500)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
+}
 
 data class DetailUiState(
     val detailUiEvent: DosenEvent = DosenEvent(),
@@ -16,6 +67,7 @@ data class DetailUiState(
         get() = detailUiEvent != DosenEvent()
 }
 
+
 fun Dosen.toDetailUiEvent(): DosenEvent {
     return DosenEvent(
         nidn =nidn,
@@ -23,6 +75,7 @@ fun Dosen.toDetailUiEvent(): DosenEvent {
         jenisKelamin = jenisKelamin
     )
 }
+
 
 fun DosenEvent.toDosenEntity(): Dosen = Dosen(
     nidn = nidn,
