9547965e7a5eb6b40334af3123cf980b3ef86d93
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-23 19:30:13 +0700

    Membuat UI untuk halaman DetailDosen (Fungsi ItemDetail dan DetailDosenView)

--- app/src/main/java/com/example/ucp_2/ui/view/dosen/DetailDosenView.kt
+++ app/src/main/java/com/example/ucp_2/ui/view/dosen/DetailDosenView.kt
@@ -2,21 +2,83 @@ package com.example.ucp_2.ui.view.dosen
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp_2.data.entity.Dosen
+import com.example.ucp_2.ui.customwidget.CustomTopAppBar
+import com.example.ucp_2.ui.viewmodel.dosen.DetailDosenViewModel
 import com.example.ucp_2.ui.viewmodel.dosen.DetailUiState
+import com.example.ucp_2.ui.viewmodel.dosen.PenyediaViewModel
 import com.example.ucp_2.ui.viewmodel.dosen.toDosenEntity
 
+@Composable
+fun DetailDosenView(
+    modifier: Modifier = Modifier,
+    viewModel: DetailDosenViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onBack: () -> Unit = { },
+    ) {
+    Scaffold(
+        topBar = {
+            CustomTopAppBar(
+                judul = "Detail Dosen",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        },
+    ) { innerPadding ->
+        val detailUiState by viewModel.detailUiState.collectAsState()
+        BodyDetailDosen(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = detailUiState,
+        )
+    }
+}
 
+@Composable
+fun ItemDetailDosen(
+    modifier: Modifier =Modifier,
+    dosen: Dosen
+
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        ))
+    {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ComponentDetailDosen(judul = "NIP", isinya = dosen.nidn)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailDosen(judul = "Nama", isinya = dosen.nama)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailDosen(judul = "JenisKelamin", isinya = dosen.jenisKelamin)
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
+    }
+
+}
 
 @Composable
 fun BodyDetailDosen(
