f4afafc47aa89295eb5930d21be293637286100f
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-23 15:07:57 +0700

    Membuat custom app bar dengan tombol "Kembali" dan judul

--- app/src/main/java/com/example/ucp_2/ui/customwidget/CustomTopAppBar.kt
+++ app/src/main/java/com/example/ucp_2/ui/customwidget/CustomTopAppBar.kt
@@ -5,33 +5,31 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-
 @Composable
-fun TopAppBar(
+// Fungsi untuk membuat custom top app bar dengan tombol kembali dan judul
+fun CustomTopAppBar(
     onBack: () -> Unit,
     showBackButton: Boolean = true,
     judul: String,
     modifier: Modifier = Modifier
 ) {
     Box(
-        modifier = modifier
-            .fillMaxWidth()
-            .statusBarsPadding(),
-        contentAlignment = Alignment.Center
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        contentAlignment = Alignment.Center // Pastikan konten di tengah
     ) {
         if (showBackButton) {
             Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
+                modifier = Modifier.fillMaxWidth(),verticalAlignment = Alignment.CenterVertically
             ) {
                 TextButton(
                     onClick = onBack,
@@ -41,16 +39,14 @@ fun TopAppBar(
                 }
                 Spacer(modifier = Modifier.weight(2f))
             }
-
-            Text(
-                text = judul,
-                fontSize = 25.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.align(Alignment.Center)
-            )
-
         }
 
+        // Menampilkan teks judul di tengah app bar
+        Text(
+            text = judul,
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.align(Alignment.Center)
+        )
     }
-
 }
