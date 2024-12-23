a361ef4b2a0a2279d1d289ca9b43fb8634ce501f
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-23 19:20:17 +0700

    Membuat UI untuk halaman HomeDosen secara keseluruhan

--- app/src/main/java/com/example/ucp_2/ui/view/dosen/HomeDosenView.kt
+++ app/src/main/java/com/example/ucp_2/ui/view/dosen/HomeDosenView.kt
@@ -1,33 +1,171 @@
 package com.example.ucp_2.ui.view.dosen
 
-
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp_2.R
 import com.example.ucp_2.data.entity.Dosen
+import com.example.ucp_2.ui.customwidget.CustomTopAppBar
+import com.example.ucp_2.ui.viewmodel.dosen.HomeDosenViewModel
+import com.example.ucp_2.ui.viewmodel.dosen.HomeUiState
+import com.example.ucp_2.ui.viewmodel.dosen.PenyediaViewModel
 
 
+@Composable
+fun HomeDosenView(
+    viewModel: HomeDosenViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddDosen: () -> Unit = { },
+    onBack: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            CustomTopAppBar(
+                judul = "Daftar Dosen",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = Modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddDosen,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Dosen"
+                )
+            }
+        }
+    ) { innerPadding ->
+        val homeUiState by viewModel.homeUiState.collectAsState()
+        BodyHomeDosenView(
+            homeUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
 
+}
+
+@Composable
+fun BodyHomeDosenView(
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+    when {
+        homeUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        homeUiState.isError -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Terjadi kesalahan: ${homeUiState.errorMessage}",
+                    color = MaterialTheme.colorScheme.error
+                )
+            }
+        }
+
+        homeUiState.listDosen.isEmpty() -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Tidak ada data dosen.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+
+        else -> {Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(900.dp)
+                .padding(top = 110.dp)
+                .background(
+                    color = colorResource(id = R.color.skyblue),
+                    shape = RoundedCornerShape(topStart = 25.dp, topEnd = 0.dp) // Rounded hanya di kiri atas
+                )
+        ) {
+                ListDosen(
+                    listDosen = homeUiState.listDosen,
+                    onClick = {
+                        onClick(it)
+                    },
+                    modifier = modifier
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ListDosen(
+    listDosen: List<Dosen>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+) {
+    LazyColumn(modifier = modifier) {
+        items(listDosen) { dosen ->
+            CardDosen(
+                dosen = dosen,
+                onClick = { onClick(dosen.nidn) }
+            )
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
