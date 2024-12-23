d300f0d12d0c5def5506222e2603067900dcf5b8
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-23 15:31:56 +0700

    Membuat beberapa data class dalam DosenViewModel

---
+++ app/src/main/java/com/example/ucp_2/ui/viewmodel/dosen/DosenViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.ucp_2.ui.viewmodel.dosen
+
+
+data class DosenEvent(
+    val nidn: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = ""
+)
+
+data class DosenUiState(
+    val dosenEvent: DosenEvent = DosenEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackBarMessage: String? = null,
+)
+
+data class FormErrorState(
+    val nidn: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null
+) {
+    fun isValid(): Boolean {
+        return nidn == null && nama == null && jenisKelamin == null
+    }
+}
