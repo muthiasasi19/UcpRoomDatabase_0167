27790b4b87ceb8d82bf3b9f3b6ef01b02e227fb3
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-23 16:10:10 +0700

    Membuat class UpdateMataKuliahViewModel, fungsi update, dan validasi

---
+++ app/src/main/java/com/example/ucp_2/ui/viewmodel/matakuliah/UpdateMataKuliahViewModel.kt
@@ -0,0 +1,72 @@
+package com.example.ucp_2.ui.viewmodel.matakuliah
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp_2.data.entity.Dosen
+import com.example.ucp_2.data.entity.MataKuliah
+import com.example.ucp_2.repository.dosen.RepositoryDosen
+import com.example.ucp_2.repository.matakuliah.LocalRepositoryMk
+import com.example.ucp_2.ui.Navigasi.DestinasiUpdateMataKuliah
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class UpdateMataKuliahViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMataKuliah: LocalRepositoryMk,
+    private val repossioryDsn: RepositoryDosen
+) : ViewModel(){
+
+    var dosentList by mutableStateOf<List<Dosen>>(emptyList())
+        private set
+
+    var updateUiState by mutableStateOf(MataKuliahUiState())
+        private set
+
+    private val _kode: String = checkNotNull(savedStateHandle[DestinasiUpdateMataKuliah.KODE_MATAKULIAH])
+
+    init {
+        viewModelScope.launch {
+            updateUiState = repositoryMataKuliah.getDetailMataKuliah(_kode)
+                .filterNotNull()
+                .first()
+                .toUiStateMataKuliah()
+        }
+        viewModelScope.launch {
+            val dosentListFromRepo = repossioryDsn.getAllDosen().first()
+            updateUiState = updateUiState.copy(dosentList = dosentListFromRepo)
+        }
+    }
+
+    fun MataKuliah.toUiStateMataKuliah(): MataKuliahUiState = MataKuliahUiState(
+        mataKuliahEvent = this.toDetailUiEvent()
+    )
+
+    fun updateState(mataKuliahEvent: MataKuliahEvent) {
+        updateUiState = updateUiState.copy(
+            mataKuliahEvent = mataKuliahEvent
+        )
+    }
+
+    fun validateFields(): Boolean {
+        val event = updateUiState.mataKuliahEvent
+        val errorState = FormErrorState(
+            kode = if (event.kode.isNotEmpty()) null else "Kode tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            sks = if (event.sks.isNotEmpty()) null else "SKS tidak boleh kosong",
+            semester = if (event.semester.isNotEmpty()) null else "Semester tidak boleh kosong",
+            jenis = if (event.jenis.isNotEmpty()) null else "Jenis tidak boleh kosong",
+            dosenPengampu = if (event.dosenPengampu.isNotEmpty()) null else "Dosen Pengampu tidak boleh kosong"
+        )
+
+        updateUiState = updateUiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+
+
+}
