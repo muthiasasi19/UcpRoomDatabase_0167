79644e01d8039ed332afdf3c888820ed71743ab6
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-23 19:25:27 +0700

    Membuat UI untuk halaman DetailDosen (Fungsi BodyDetail dan ComponentDetail)

---
+++ app/src/main/java/com/example/ucp_2/ui/view/dosen/DetailDosenView.kt
@@ -0,0 +1,72 @@
+package com.example.ucp_2.ui.view.dosen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ucp_2.ui.viewmodel.dosen.DetailUiState
+import com.example.ucp_2.ui.viewmodel.dosen.toDosenEntity
+
+
+
+@Composable
+fun BodyDetailDosen(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState = DetailUiState(),
+
+    ) {
+    when {
+        detailUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+        detailUiState.isUiEventNotEmpty -> {
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailDosen(
+                    dosen = detailUiState.detailUiEvent.toDosenEntity(),
+                    modifier = Modifier
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ComponentDetailDosen(
+    judul: String,
+    isinya: String
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
