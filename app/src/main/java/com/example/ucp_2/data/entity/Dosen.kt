89fe507a2015bfb58b9d224ceee7f42177efb40b
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-23 14:35:12 +0700

    Membuat data Class Dosen dan MataKuliah

--- app/src/main/java/com/example/ucp_2/data/entity/Dosen.kt
+++ app/src/main/java/com/example/ucp_2/data/entity/Dosen.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "dosen")
 data class Dosen(
     @PrimaryKey
-    val nidn: String, // yg Primary key hanya satu baris dibawah @PrimaryKey saja // knp nim tidak int? karena datanya tidak dikomputasi nantinya
-    val nama: String,
-    val jenisKelamin: String
+    val nidn : String,
+    val nama : String,
+    val jenisKelamin : String,
 )
--- app/src/main/java/com/example/ucp_2/data/entity/Matakuliah.kt
+++ app/src/main/java/com/example/ucp_2/data/entity/Matakuliah.kt
@@ -3,13 +3,14 @@ package com.example.ucp_2.data.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "matakuliah")
-data class Matakuliah(
+
+@Entity (tableName = "matakuliah")
+data class MataKuliah(
     @PrimaryKey
-    val kode: String,
-    val namamk: String,
-    val sks: String,
-    val semester: String,
-    val jenismk: String,
-    val dosenpengampu: String
+    val kode : String,
+    val nama : String,
+    val sks : String,
+    val semester : String,
+    val jenis : String,
+    val dosenPengampu : String
 )
